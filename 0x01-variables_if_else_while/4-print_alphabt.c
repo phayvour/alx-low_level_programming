b0da93b501d2523acc654566bc4962706f50123d
phayvour <phaymax22@gmail.com>
2022-12-09 17:53:25 -0800

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - prints whether the integer is positive, negative or zero
+ * Return: Always 0 if successful
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
