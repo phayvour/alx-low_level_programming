d8d9a6ec4e369f66ab598c61f65a94b7379c981c
phayvour <phaymax22@gmail.com>
2022-12-09 18:01:13 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - prints whether the integer is positive, negative or zero
+ * Return: Always 0 if successful
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
