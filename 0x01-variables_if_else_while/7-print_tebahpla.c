e026445becdfa0c16b46eb2264433d77cfcaeaf4
phayvour <phaymax22@gmail.com>
2022-12-09 18:20:20 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - prints whether the integer is positive, negative or zero
+ * Return: Always 0 if successful
+ *
+ */
+int main(void)
+{
+	char c = 'z';
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
