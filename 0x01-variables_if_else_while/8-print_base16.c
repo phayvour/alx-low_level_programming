0740da339ae765886ab1535c729430cf8866df79
phayvour <phaymax22@gmail.com>
2022-12-09 18:56:20 -0800

    Persistent

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - prints whether the integer is positive, negative or zero
+ * Return: Always 0 if successful
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		if (x < 10)
+		{
+			putchar(x + '0');
+		} else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - prints whether the integer is positive, negative or zero
+ * Return: Always 0 if successful
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
