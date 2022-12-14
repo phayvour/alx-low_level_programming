9bf606f53921ed4ec2052956156066673fc5780b
phayvour <phaymax22@gmail.com>
2022-12-13 19:46:07 -0800

    test if alphabet case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lower alphabet characters
+ *
+ * @c: character to be tested
+ *
+ * Return: 1 if its an alphabet case, otherwise return 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
