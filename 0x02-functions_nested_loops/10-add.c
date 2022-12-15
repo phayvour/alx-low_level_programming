29fc1705f63b250ce974f2b3a03f43fa3513dc49
phayvour <phaymax22@gmail.com>
2022-12-15 13:21:16 -0800

    Yay! addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - perform addition operation
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result if successful
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
