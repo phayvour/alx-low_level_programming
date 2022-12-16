74dc33b47533114635bfb148989851d9b5257f59
phayvour <phaymax22@gmail.com>
2022-12-16 06:34:24 -0800

    be fruitful and multiply :)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplication
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
