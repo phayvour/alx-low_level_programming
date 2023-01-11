e6aa4aacb31f52afac554c37638608c0e2850f76
phayvour <phaymax22@gmail.com>
2023-01-10 19:14:03 -0800

    let me show you greatness

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,97 @@
+#include "main.h"
+#include <stdlib.h>
+
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
+/**
+ * word_len - locates the index marking the end
+ *  of the first word within the string
+ *  @str: the string to be checked
+ *
+ *  Return: returns the index marking of the end
+ *  of the initial word pointed to by str
+ */
+int word_len(char *str)
+{
+	int i = 0, len = 0;
+
+	while (*(str + i) && *(str + i) != ' ')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * count_words - counts the number of words contained in a string
+ * @str: string in question
+ *
+ * Return: return the number of words
+ */
+int count_words(char *str)
+{
+	int index = 0, words = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+	return (words);
+}
+
+/**
+ * strtow - splits a string into words.
+ * @str: string in question.
+ *
+ * Return: returns NULL if str = NULL or str = "" or function fails
+ * otherwise it returns a pointer to an array of strings (words).
+ */
+char **strtow(char *str)
+{
+	int index = 0, words, w, letters, l;
+	char **strings;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+
+	strings = malloc(sizeof(char *) * words + 1);
+	if (strings == NULL)
+		return (NULL);
+	for (w = 0; w <  words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+		strings[w] = malloc(sizeof(char) * letters + 1);
+
+		if (strings[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(strings[w]);
+			free(strings);
+			return (NULL);
+		}
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
+
+		strings[w][l] = '\0';
+	}
+	strings[w] = NULL;
+
+	return (strings);
+}
