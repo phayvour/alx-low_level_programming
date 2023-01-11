20023afdd251de9f114927e82a041d10e1e8e01e
phayvour <phaymax22@gmail.com>
2023-01-11 03:30:03 -0800

    bureau de change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,59 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
 /**
- * main - minimum number of coins to make change for an amount of money
- * @argc: array size
- * @argv: content of the array
- *
- * Return: returns 0 on success
+ * main - prints the min number of coins to make change
+ * for an amount of money
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int value, c;
+	int total, count;
+	unsigned int i;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
 
-	c = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	value = atoi(argv[1]);
-	if (value < 0)
+
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
 	{
-		printf("%d\n", 0);
-		return (0);
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
 	}
-	if (value % 25 >= 0)
+	else
 	{
-		c += value / 25;
-		value = value % 25;
+		printf("Error\n");
+		return (1);
 	}
 
-	if (value % 10 >= 0)
-	{
-		c += value / 10;
-		value = value % 10;
-	}
-
-	if (value % 5 >= 0)
-	{
-		c += value / 5;
-		value = value % 5;
-	}
-	if (value % 2 >= 0)
-	{
-		c += value / 2;
-		value = value % 2;
-	}
-
-	if (value % 1 >= 0)
-	{
-		c += value / 1;
-	}
-	printf("%d\n", c);
+	printf("%d\n", count);
 	return (0);
-
 }
